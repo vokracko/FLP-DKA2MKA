1ba6c0e7d848ae976171ccde1c19f13ea28b77e1
Lukáš Vokráčko <lukas@vokracko.cz>
2016-03-14 22:43:36 +0100

    better output

--- test.sh
+++ test.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 for input in $(ls ./test/*.in) ; do
-	diff -q <(./dka-2-mka -i $input) <($input)
-	st=$?
-
-	diff -q <(./dka-2-mka -t $input) <($(basename $input .in).out)
-	nd=$?
-
-	if [[ ! $st -eq 0 ]] || [[ ! $nd -eq 0 ]]; then
-		printf "$input: -i %d -t %d\n" $st $nd
+	diff -q <(./dka-2-mka -i $input | sort) <(cat $input | sort) 2>/dev/null >&2
+	if [ $? -eq 0 ] ; then
+		st="\033[1;32mPASS\033[0m"
+	else
+		st="\033[1;31mFAIL\033[0m"
 	fi
+
+	diff -q <(./dka-2-mka -t $input | sort) <(cat $(dirname $input)/$(basename $input .in).out | sort) 2>/dev/null >&2
+	if [ $? -eq 0 ] ; then
+		nd="\033[1;32mPASS\033[0m"
+	else
+		nd="\033[1;31mFAIL\033[0m"
+	fi
+
+	echo -e "-i $st -t $nd\t$input"
 done
