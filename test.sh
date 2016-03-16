1c72d7647f589df767effb4c369466d1df4bd022
Lukáš Vokráčko <lukas@vokracko.cz>
2016-03-16 20:10:23 +0100

    save diff

--- test.sh
+++ test.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+ghc dka-2-mka.hs
+
+rm -rf test/diffs/*/*
+
 for input in $(ls ./test/*.in) ; do
-	diff -q <(./dka-2-mka -i $input | sort) <(cat $input | sort) 2>/dev/null >&2
+	diff <(./dka-2-mka -i $input | sort) <(cat $input | sort) > test/diffs/i/$(basename $input .in).diff
 	if [ $? -eq 0 ] ; then
 		st="\033[1;32mPASS\033[0m"
 	else
 		st="\033[1;31mFAIL\033[0m"
 	fi
 
-	diff -q <(./dka-2-mka -t $input | sort) <(cat $(dirname $input)/$(basename $input .in).out | sort) 2>/dev/null >&2
+	diff <(./dka-2-mka -t $input | sort) <(cat $(dirname $input)/$(basename $input .in).out | sort) > test/diffs/t/$(basename $input .in).diff
 	if [ $? -eq 0 ] ; then
 		nd="\033[1;32mPASS\033[0m"
 	else
@@ -17,3 +21,5 @@ for input in $(ls ./test/*.in) ; do
 
 	echo -e "-i $st -t $nd\t$input"
 done
+
+find test -empty -exec rm {} \;
