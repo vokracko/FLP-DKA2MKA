07b94d97ce9101d5271187c439c6aad79963ef61
Lukáš Vokráčko <lukas@vokracko.cz>
2016-03-26 19:55:53 +0100

    tests moved to new repo

--- test/show.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fstdraw --title="Correct $(basename $1)" ${1} | xdot > /dev/null 2>&1 &
-
-if [[ "$1" = *"original"* ]] ; then
-	fstdraw --title="My $(basename $1)" ${1/original/res}.plain.fst | xdot > /dev/null 2>&1 &
-else
-	fstdraw --title="My $(basename $1)" ${1/minimal/res}.minimal.fst | xdot > /dev/null 2>&1 &
-fi
